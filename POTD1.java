012da8f8fed3ba5f4ecb98e571b5e8c001578278
Sharad K <76866661+Sk783@users.noreply.github.com>
2023-09-16 20:57:34 +0530

    Add files via upload

---
+++ POTD1.java
@@ -0,0 +1,14 @@
+public class POTD1 {
+    public static String reverseWords(String str){
+        String[] words = str.split(" ");
+        String res = "";
+        for (int i = words.length-1; i>=0; i--){
+                res+=words[i]+" ";
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        String str = "Welcome to coding Ninjas";
+        System.out.println(reverseWords(str));
+    }
+}
